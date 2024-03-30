cd818a19bc89455c02cf2138a196656a7e62b1ee
MhaamadAli <m.alialaawar@gmail.com>
2024-03-30 19:52:24 +0200

    created all migrations for all the DataBase

---
+++ database/migrations/2024_03_30_173757_create_stations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('manager_id')->unique()->nullable();
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 10, 8);
+            $table->string('address');
+            $table->text('operating_hours');
+            $table->text('facilities')->nullable();
+            $table->enum('service_status', ['operational', 'maintenance', 'closed']);
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('manager_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stations');
+    }
+};
